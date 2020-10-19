b67a7642bedfbea959728d8986e7555f935ced17
beinguxman <64712829+beinguxman@users.noreply.github.com>
2020-10-20 01:10:12 +0500

    232

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+// import React from "react";
 
-const Footer = () => {
-    return (
-     <>
-        {/* Site footer  */}
- <footer className="bg-light text-center">
-   <p>Copy Rights © 2019. All rights reserved  Designed by <b>InfinitySolDevelopers</b></p>
- </footer>
-     </>
-);
-};
-export default Footer;
+// const Footer = () => {
+//     return (
+//      <>
+//         {/* Site footer  */}
+//  <footer className="bg-light text-center">
+//    <p>Copy Rights © 2019. All rights reserved  Designed by <b>InfinitySolDevelopers</b></p>
+//  </footer>
+//      </>
+// );
+// };
+// export default Footer;
--- src/app.jsx
+++ src/app.jsx
@@ -19,7 +19,7 @@ const App = () =>{
             <Redirect to="/" />  
             <Home />
         </Switch>
-        <Footer/>
+        {/* <Footer/> */}
         </>
     );
 };
