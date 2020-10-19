1888e0123b330f4f8681c0f1ef5fed1d63706e0e
beinguxman <64712829+beinguxman@users.noreply.github.com>
2020-10-19 21:11:29 +0500

    Updates

---
+++ precache-manifest.f17a8b4933b797363b4dbab9dfcacaca.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd94f45106b790e7d5a6de070591494",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "42abda0cd2c2eca4d6db",
+    "url": "/reactjs/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "7aebd894fdf4d9044dc5",
+    "url": "/reactjs/static/css/main.81e4b9d8.chunk.css"
+  },
+  {
+    "revision": "42abda0cd2c2eca4d6db",
+    "url": "/reactjs/static/js/2.57f98c8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs/static/js/2.57f98c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aebd894fdf4d9044dc5",
+    "url": "/reactjs/static/js/main.9c92ec4c.chunk.js"
+  },
+  {
+    "revision": "23319981ec5572108c4c",
+    "url": "/reactjs/static/js/runtime-main.2035eb9f.js"
+  },
+  {
+    "revision": "b72c44017b2f49da6f7bee7fa1f567cd",
+    "url": "/reactjs/static/media/andriod.b72c4401.jpg"
+  },
+  {
+    "revision": "22116dcf3ef48050ee7e209cc7cba1bb",
+    "url": "/reactjs/static/media/bg12.22116dcf.jpg"
+  },
+  {
+    "revision": "2e1ee53a6f8833424458c8f24459e1b8",
+    "url": "/reactjs/static/media/bg2.2e1ee53a.jpg"
+  },
+  {
+    "revision": "85a4eed3a14ac73947eebbdf73e830d5",
+    "url": "/reactjs/static/media/img1.85a4eed3.jpg"
+  },
+  {
+    "revision": "d7053ee5fab8aa3567da0bebb0b9d23d",
+    "url": "/reactjs/static/media/marketing.d7053ee5.jpg"
+  },
+  {
+    "revision": "6bebb7ab3a4bde550ef309526fb3ab60",
+    "url": "/reactjs/static/media/software.6bebb7ab.png"
+  },
+  {
+    "revision": "57e269d772efb557362226367e9e2a63",
+    "url": "/reactjs/static/media/web1.57e269d7.jpeg"
+  },
+  {
+    "revision": "573b46cf84402335c3ce89e7cd573263",
+    "url": "/reactjs/static/media/web2.573b46cf.jpg"
+  }
+]);
